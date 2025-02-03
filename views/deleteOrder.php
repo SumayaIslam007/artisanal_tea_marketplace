c0b8d802b2a8be83475a9fa2f5e817064362069c
nadiyanoor955 <nadiyanoor2020@gmail.com>
2025-02-03 09:53:14 +0600

    Add files via upload

---
+++ views/deleteOrder.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+
+</head>
+<body>
+    <form action="../controllers/deleteOrder_Control.php" method="POST">
+        <h2>Delete Order</h2>
+        <label for="OrderID">Enter Order ID to Delete:</label>
+        <input type="number" id="OrderID" name="OrderID" placeholder="Enter Order ID">
+        <button type="submit">Delete Order</button>
+    </form>
+</body>
+</html>
